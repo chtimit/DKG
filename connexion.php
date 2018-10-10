65a476a1283ffff71db81c08027056469f72dea1
Kenzuyo <david.guetat35@gmail.com>
2018-10-10 15:25:39 +0200

    Add files via upload
    CSS HTML mis à jour

--- connexion.php
+++ connexion.php
@@ -1,21 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>connexion</title>
-</head>
+<link rel="stylesheet" type="text/css" href="style/style.css">
+<header>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Connexion</title>
+	<a href="accueil.html"><img src="src/log12.png"> </a>
+
+</header>
 <body>
-	<div>
+	<div class="formconnexion">
 		<form method='POST' action='action_connexion.php'>
 			<div>
-				<input type="text" name="pseudo" <?php if (isset($_GET['error']) && $_GET['error'] == "pseudo") { echo ("placeholder='!erreur de saisie' style='border-style: solid; border-color: red;'");} if (isset($_GET['error']) && $_GET['error'] == "pseudo2") { echo ("placeholder='!identifiant incorect' style='border-style: solid; border-color: red;'"); } ?>>
-				<label>identifiant</label>
+				<label>Identifiant</label>
+				<input type="text" name="pseudo" class="champsblanc" <?php if (isset($_GET['error']) && $_GET['error'] == "pseudo") { echo ("placeholder='erreur de saisie' style='border-style: solid; border-color: red;'");} if (isset($_GET['error']) && $_GET['error'] == "pseudo2") { echo ("placeholder='!identifiant incorect' style='border-style: solid; border-color: red;'"); } ?>>	
 			</div><br>
 			<div>
-				<input type="password" name="pwd" <?php if (isset($_GET['error']) && $_GET['error'] == "pwd") { echo ("placeholder='!erreur de saisie' style='border-style: solid; border-color: red;'");} if (isset($_GET['error']) && $_GET['error'] == "pwd2") { echo ("placeholder='!mot de passe incorect' style='border-style: solid; border-color: red;'"); } ?>>
-				<label>mot de passe</label>
+				<label>Mot de passe</label>
+				<input type="password" name="pwd" class="champsblanc" <?php if (isset($_GET['error']) && $_GET['error'] == "pwd") { echo ("placeholder='erreur de saisie' style='border-style: solid; border-color: red;'");} if (isset($_GET['error']) && $_GET['error'] == "pwd2") { echo ("placeholder='!mot de passe incorect' style='border-style: solid; border-color: red;'"); } ?>>
 			</div>
-			<button type="submit">valider</button>
+			<button type="submit" class="formvalider">Connexion</button>
 		</form>
+	<div class="nonmembre">Pas encore membre ? <a href="inscrption.php" class="lieninscris">Inscrivez vous</a></div>
 	</div>
 </body>
 </html>
