83743f8722d9e520ad53af3c9f55da61562b2ec5
goulven <goulven.cadot@gmail.com>
2018-10-11 20:03:04 +0200

    Merge branch 'master' of https://github.com/chtimit/DKG

---
+++ action_evenement.php
@@ -0,0 +1,43 @@
+<?php
+
+	session_start();
+
+	$host = "localhost";
+	$dbname = "workshop";
+	$id_db = "root";
+	$mdp_db = "";
+
+	try
+	{
+	    // create connexion
+		$dbh = new PDO('mysql:host='.$host.';dbname='.$dbname, $id_db, $mdp_db);
+	}
+
+	catch (PDOException $e)
+	{
+	     print "Erreur !: " . $e->getMessage() . "<br/>";
+	}
+
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id_can']) && isset($_POST['action']))
+	{
+		if ($_POST['action'] == 'ac')
+		{
+			$sql = "UPDATE candidature set etat='accepte' WHERE id_candidature=".$_POST['id_can'];
+			$dbh->query($sql);
+		}
+		if ($_POST['action'] == 're')
+		{
+			$sql = "UPDATE candidature set etat='refuse' WHERE id_candidature=".$_POST['id_can'];
+			$dbh->query($sql);
+		}
+		if ($_POST['action'] == 'an')
+		{
+			$sql = "DELETE FROM candidature WHERE id_candidature=".$_POST['id_can'];
+			$dbh->query($sql);
+		}
+		header('location: evenement.php?id='.$_POST['id']);
+		exit();
+	}else{ echo "une erreure s'est produite!";}
+
+	$dbh = null;
+?>
--- evenement.php
+++ evenement.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>consulter</title>
+	<title>Evenement</title>
 	<?php
 
 	session_start();
@@ -24,8 +24,22 @@
 
 	function result($dbh)
 	{
-		$sql = "SELECT evenement.id_evenement, evenement.nom_evenement, categorie.categorie, utilisateur.pseudo_utilisateur, evenement.cp_evenement, evenement.ville_evenement, evenement.description_evenement FROM evenement LEFT JOIN categorie ON categorie.id_categorie=evenement.categorie LEFT JOIN utilisateur ON utilisateur.id_utilisateur=evenement.id_utilisateur WHERE evenement.id_evenement='".$_GET['id']."'";
+		$sql = "SELECT evenement.id_evenement, evenement.id_utilisateur, evenement.nom_evenement, categorie.categorie, utilisateur.pseudo_utilisateur, evenement.cp_evenement, evenement.ville_evenement, evenement.description_evenement FROM evenement LEFT JOIN categorie ON categorie.id_categorie=evenement.categorie LEFT JOIN utilisateur ON utilisateur.id_utilisateur=evenement.id_utilisateur WHERE evenement.id_evenement='".$_GET['id']."'";
 		return ($dbh->query($sql));
+	}
+
+	function verif_libre($dbh, $id, $id_util)
+	{
+		$sql = "SELECT * FROM candidature WHERE id_evenement=".$id;
+		$res = $dbh->query($sql);
+		foreach ($res as $can) {
+			if ($can['id_utilisateur'] == $id_util)
+			{
+				return (false);
+				header('location: evenement.php?id='.$id.'&error=dej');
+				exit();
+			}
+		}
 	}
 
 	if ($_SERVER['REQUEST_METHOD'] != 'GET' || !isset($_GET['id']) || $_GET['id'] < 1)
@@ -33,6 +47,22 @@
 		
 		header('location: accueil.php');
 		exit();
+	}
+
+	if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id_util']))
+	{
+		if ($_GET['id_util'] == 0)
+		{
+			header('location: connexion.php');
+			exit();
+		}
+		else
+		{
+			if (verif_libre($dbh, $_GET['id'], $_GET['id_util'])) {
+				$sql = "INSERT INTO candidature(id_evenement, id_utilisateur) values ('".$_GET['id']."', '".$_GET['id_util']."')";
+				$dbh->query($sql);
+			}
+		}
 	}
 
 	$result = result($dbh);
@@ -62,8 +92,51 @@
 		<form method='GET' action="">
 			<input type="hidden" name="id" value=<?php echo ("'".$eve['id_evenement']."'") ?>>
 			<input type="hidden" name="id_util" value=<?php if (isset($_SESSION['id_compte'])){echo ("'".$_SESSION['id_compte']."'");}else{echo ("'0'");} ?>>
-			<button type='submit'>Proposer sa candidature</button>
+			<?php if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['error']) && $_GET['error'] == 'dej') { ?>
+				<label type='error' style='border-style: solid;border-color: red'>déja candidat</label>
+			<?php } else { ?>
+				<button type='submit'>Proposer sa candidature</button>
+			<?php }	?>
 		</form>
+	</div>
+	<div>
+		<?php  
+		$sql = "SELECT utilisateur.pseudo_utilisateur, candidature.etat, candidature.id_candidature, candidature.id_utilisateur FROM candidature LEFT JOIN utilisateur ON candidature.id_utilisateur=utilisateur.id_utilisateur WHERE candidature.id_evenement=".$_GET['id']." ORDER BY etat";
+		$res = $dbh->query($sql);
+		foreach ($res as $can) { ?>
+			<div>
+				<label><?php echo ($can['pseudo_utilisateur']) ?></label>
+				<label><?php echo ($can['etat']) ?></label>
+				<?php if (!isset($_SESSION['login']) || ($_SESSION['id_compte'] != $eve['id_utilisateur'] && $_SESSION['id_compte'] != $can['id_utilisateur']) ||  ($can['etat'] == 'accepte' || $can['etat'] == 'refuse')) { ?>
+
+
+				<?php } else if ($_SESSION['id_compte'] == $eve['id_utilisateur']){ ?>
+
+					<form method="POST" action="action_evenement.php">
+						<input type="hidden" name="id_can" value=<?php echo ($can['id_candidature']); ?>>
+						<input type="hidden" name="action" value="ac">
+						<input type="hidden" name="id" value=<?php echo ($_GET['id']); ?>>
+						<button type="submit">Accepter</button>
+					</form>
+					<form method="POST" action="action_evenement.php">
+						<input type="hidden" name="id_can" value=<?php echo ($can['id_candidature']); ?>>
+						<input type="hidden" name="action" value="re">
+						<input type="hidden" name="id" value=<?php echo ($_GET['id']); ?>>
+						<button type="submit">Refuser</button>
+					</form>
+					
+				<?php } else { ?>
+
+					<form method="POST" action="action_evenement.php">
+						<input type="hidden" name="id_can" value=<?php echo ($can['id_candidature']); ?>>
+						<input type="hidden" name="action" value="an">
+						<input type="hidden" name="id" value=<?php echo ($_GET['id']); ?>>
+						<button type="submit">Annuler</button>
+					</form>
+
+				<?php } ?>
+			</div>
+		<?php } ?>
 	</div>
 </body>
 <?php $dbh = null; ?>
